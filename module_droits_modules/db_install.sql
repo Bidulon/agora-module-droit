0238769c6428e64c5d934cfc632afb3b562442b0
PLANCON Sylvain <sanplomb@gmail.com>
2012-08-23 01:09:24 +0200

    [MODULE_DROITS_MODULE] Ajout de fichiers d'origine modifié et du fichier SQL de mise à jour de la BDD

--- module_droits_modules/db_install.sql
+++ module_droits_modules/db_install.sql
@@ -1,15 +1,3 @@
 INSERT INTO `gt_module` (`nom`, `module_dossier_fichier`) VALUES ('droits_modules', 'module_droits_modules');
 
-CREATE TABLE `gt_jointure_droits_module_utilisateur`  (
-  `id_espace` INT NOT NULL ,
-  `nom_module` TINYTEXT ,
-  `id_utilisateur` INT NOT NULL,
-  `droit` INT NOT NULL
-);
-
-CREATE TABLE `gt_jointure_droits_module_groupe`  (
-  `id_espace` INT NOT NULL ,
-  `nom_module` TINYTEXT ,
-  `id_groupe` INT NOT NULL,
-  `droit` INT NOT NULL
-);
+ALTER TABLE `gt_module` ADD COLUMN `id_module` INT NOT NULL FIRST;
---
+++ traduction/modules_fr/module_droits_modules.php
@@ -0,0 +1,10 @@
+<?php
+
+$trad["DROITS_MODULES_nom_module"] = "Droits sur les modules";
+$trad["DROITS_MODULES_nom_module_header"] = "Droits sur les modules";
+$trad["DROITS_MODULES_description_module"] = "Gestion des droits d'accès aux modules";
+
+$trad['DROITS_MODULES_liste_modules'] = "Liste des modules de l'espace";
+
+$trad['Aucun'] = "Aucun";
+?>
