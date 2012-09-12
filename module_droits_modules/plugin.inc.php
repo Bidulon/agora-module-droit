7be16d570e4a43a7040ce528515ccc3b13fbd736
PLANCON Sylvain <sanplomb@gmail.com>
2012-09-13 01:29:05 +0200

    Ajout de la fonction permettant de vérifier l'accés en lecture à un module

--- module_droits_modules/plugin.inc.php
+++ module_droits_modules/plugin.inc.php
@@ -20,4 +20,27 @@ if (!function_exists("droit_ecriture_module")) {
 		return $ecriture_ok;
 	}
 }
+
+if (!function_exists("droit_acces_module")) {
+    function droit_acces_module() {
+        $groupes_utilisateur = groupes_users($_SESSION["espace"]["id_espace"], $_SESSION['user']['id_utilisateur']);
+
+        // Récupération de l'id_module
+        $module_infos = db_tableau("SELECT * FROM gt_module WHERE nom='".MODULE_NOM."'");
+        if (!is_array($module_infos)) { return false; }
+
+        $module_infos = $module_infos[0];
+
+        $groupes_1	= objet_affectations(array('type_objet' => 'module'), $module_infos['id_module'], "groupes", 1);
+        if (is_null($groupes_1)) { return false; }
+
+        // Parcours de $groupes_1 pour vérifier si un des groupes de l'utilisateur à le droit d'écrire
+        $ecriture_ok = false;
+        for ($i=0; $i < count($groupes_1) && !$ecriture_ok; $i++) {
+            $ecriture_ok = $ecriture_ok || in_array($groupes_1[$i]['id_groupe'], array_keys($groupes_utilisateur));
+        }
+        return $ecriture_ok;
+    }
+}
+
 ?>
